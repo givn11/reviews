d972319f57bd828c03f9367d7777920257c657a1
Igor <giv_81@mail.ru>
2019-01-16 15:14:13 +0300

    first commit

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+<?php
+if(isset($_GET['api'])){
+	$api_key = strip_tags(trim(htmlspecialchars($_GET['api'])));
+}
+$config = array(
+	//'api_key' => 'Enter Your API key',
+	'uagent' => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; ru; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12'
+);
+//$api_key = 'Enter Your API key';
